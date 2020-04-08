28efb9d9e90523a98ea95f4fbd04a48412125f09
Martin Guillon <martin.guillon@akylas.fr>
2020-04-08 09:40:03 +0200

    park: way_pixels +noname for polygons

--- layers/park/layer.sql
+++ layers/park/layer.sql
@@ -1,57 +1,60 @@
 -- etldoc: layer_park[shape=record fillcolor=lightpink, style="rounded,filled",
 -- etldoc:     label="layer_park |<z6> z6 |<z7> z7 |<z8> z8 |<z9> z9 |<z10> z10 |<z11> z11 |<z12> z12|<z13> z13|<z14> z14+" ] ;
 
-CREATE OR REPLACE FUNCTION layer_park(bbox geometry, zoom_level int, pixel_width numeric)
-RETURNS TABLE(osm_id bigint, geometry geometry, class text, name text, tags hstore, rank int) AS $$
-    SELECT osm_id, geometry, class, name, tags, rank
+CREATE OR REPLACE FUNCTION layer_park(bbox geometry, zoom_level int, pixel_width numeric, pixel_height numeric)
+RETURNS TABLE(osm_id bigint, geometry geometry, 
+    osmId bigint,
+    class text, name text, tags hstore, rank int, way_pixels bigint) AS $$
+    SELECT osm_id, geometry, osm_id as osmId, class, name, tags, rank,way_pixels
     FROM (
     SELECT osm_id, geometry,
         COALESCE(NULLIF(boundary, ''), NULLIF(leisure, '')) AS class,
-        name, tags,
-        NULL::int as rank
+        NULL::text AS name, tags,
+        NULL::int as rank,
+        (area/NULLIF(zoom_level::real*pixel_width::real*pixel_height::real,0))::bigint AS way_pixels
         FROM (
         -- etldoc: osm_park_polygon_gen8 -> layer_park:z6
-        SELECT osm_id, geometry, name, tags, leisure, boundary
+        SELECT osm_id, geometry, NULL::text AS name, tags, leisure, boundary, area
         FROM osm_park_polygon_gen8
         WHERE zoom_level = 6 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_park_polygon_gen7 -> layer_park:z7
-        SELECT osm_id, geometry, name, tags, leisure, boundary
+        SELECT osm_id, geometry, NULL::text AS name, tags, leisure, boundary, area
         FROM osm_park_polygon_gen7
         WHERE zoom_level = 7 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_park_polygon_gen6 -> layer_park:z8
-        SELECT osm_id, geometry, name, tags, leisure, boundary
+        SELECT osm_id, geometry, NULL::text AS name, tags, leisure, boundary, area
         FROM osm_park_polygon_gen6
         WHERE zoom_level = 8 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_park_polygon_gen5 -> layer_park:z9
-        SELECT osm_id, geometry, name, tags, leisure, boundary
+        SELECT osm_id, geometry, NULL::text AS name, tags, leisure, boundary, area
         FROM osm_park_polygon_gen5
         WHERE zoom_level = 9 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_park_polygon_gen4 -> layer_park:z10
-        SELECT osm_id, geometry, name, tags, leisure, boundary
+        SELECT osm_id, geometry, NULL::text AS name, tags, leisure, boundary, area
         FROM osm_park_polygon_gen4
         WHERE zoom_level = 10 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_park_polygon_gen3 -> layer_park:z11
-        SELECT osm_id, geometry, name, tags, leisure, boundary
+        SELECT osm_id, geometry, NULL::text AS name, tags, leisure, boundary, area
         FROM osm_park_polygon_gen3
         WHERE zoom_level = 11 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_park_polygon_gen2 -> layer_park:z12
-        SELECT osm_id, geometry, name, tags, leisure, boundary
+        SELECT osm_id, geometry, NULL::text AS name, tags, leisure, boundary, area
         FROM osm_park_polygon_gen2
         WHERE zoom_level = 12 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_park_polygon_gen1 -> layer_park:z13
-        SELECT osm_id, geometry, name, tags, leisure, boundary
+        SELECT osm_id, geometry, NULL::text AS name, tags, leisure, boundary, area
         FROM osm_park_polygon_gen1
         WHERE zoom_level = 13 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_park_polygon -> layer_park:z14
-        SELECT osm_id, geometry, name, tags, leisure, boundary
+        SELECT osm_id, geometry, NULL::text AS name, tags, leisure, boundary, area
         FROM osm_park_polygon
         WHERE zoom_level >= 14 AND geometry && bbox
     ) AS park_polygon
@@ -66,7 +69,8 @@ RETURNS TABLE(osm_id bigint, geometry geometry, class text, name text, tags hsto
                (CASE WHEN boundary = 'national_park' THEN true ELSE false END) DESC,
                (COALESCE(NULLIF(tags->'wikipedia', ''), NULLIF(tags->'wikidata', '')) IS NOT NULL) DESC,
                area DESC
-        )::int AS "rank"
+        )::int AS "rank",
+        (area/NULLIF(zoom_level::real*pixel_width::real*pixel_height::real,0))::bigint AS way_pixels
         FROM (
         -- etldoc: osm_park_polygon_gen8 -> layer_park:z6
         SELECT osm_id, geometry_point, name, tags, leisure, boundary, area
