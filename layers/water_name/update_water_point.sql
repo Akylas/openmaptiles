9b131c52f4ab8e04d0bd8b6f24688398443567f6
Martin Guillon <martin.guillon@akylas.fr>
2020-04-08 09:47:09 +0200

    water_name: way_pixe;s

--- layers/water_name/update_water_point.sql
+++ layers/water_name/update_water_point.sql
@@ -15,7 +15,7 @@ CREATE MATERIALIZED VIEW osm_water_point AS (
         wp.ele
     FROM osm_water_polygon AS wp
     LEFT JOIN lake_centerline ll ON wp.osm_id = ll.osm_id
-    WHERE ll.osm_id IS NULL AND wp.name <> ''
+    WHERE ll.osm_id IS NULL AND wp.name <> '' AND wp.amenity <> 'fountain'
 ) /* DELAY_MATERIALIZED_VIEW_CREATION */;
 CREATE INDEX IF NOT EXISTS osm_water_point_geometry_idx ON osm_water_point USING gist (geometry);
 
