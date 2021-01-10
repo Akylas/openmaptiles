65aff6e6803f9572be1b9d0d0212a07089e8fa24
Martin Guillon <martin.guillon@akylas.fr>
2021-01-10 15:14:07 +0100

    remove the wikipedia rank rule it makes no sense and creates unwanted preferences

--- layers/mountain_peak/mountain_peak.sql
+++ layers/mountain_peak/mountain_peak.sql
@@ -5,14 +5,14 @@ CREATE OR REPLACE FUNCTION layer_mountain_peak (bbox geometry, zoom_level intege
     RETURNS TABLE (
                 osm_id   bigint,
                 geometry geometry,
-        osmid bigint,
+                osmid bigint,
                 name     text,
-        wikidata text,
-        wikipedia text,
+                wikidata text,
+                wikipedia text,
                 class    text,
                 tags     hstore,
                 ele      int,
-        summitcross integer,
+                summitcross integer,
                 "rank"   int
             )
     AS $$
@@ -34,42 +34,31 @@ SELECT
 FROM (
         SELECT
             osm_id,
-                geometry,
-                name,
+            geometry,
+            name,
             wikidata,
             wikipedia,
-                tags,
-                substring(ele FROM E'^(-?\\d+)(\\D|$)')::int AS ele,
+            tags,
+            substring(ele FROM E'^(-?\\d+)(\\D|$)')::int AS ele,
             summitcross,
-            row_number() OVER (PARTITION BY LabelGrid (geometry, 100 * pixel_width) ORDER BY (( CASE WHEN ele IS NOT NULL
+            row_number() OVER (
+                PARTITION BY LabelGrid (geometry, 100 * pixel_width) 
+                ORDER BY (
+                    (CASE WHEN ele IS NOT NULL
                     AND ele ~ E'^-?\\d{1,4}(\\D|$)' THEN
-                    substring(ele FROM E'^(-?\\d+)(\\D|$)')::int
-                ELSE
-                    0
-                END) + ( CASE WHEN ele IS NOT NULL
-                AND NULLIF (wikipedia, '') IS NOT NULL THEN
-                100
-            ELSE
-                0
-            END) + ( CASE WHEN ele IS NOT NULL
-            AND NULLIF (name, '') IS NOT NULL THEN
-            10000
-        ELSE
-            0
-        END)) DESC)::int AS "rank"
-FROM
-    osm_peak_point
-WHERE
-    geometry && bbox) AS ranked_peaks
-WHERE (zoom_level BETWEEN 7 AND 9
-    AND rank <= 1)
-    OR (zoom_level BETWEEN 9 AND 11
-        AND rank <= 3)
-    OR (zoom_level BETWEEN 11 AND 14
-        AND rank <= 5)
-    OR (zoom_level >= 14)
-ORDER BY
-    "rank" ASC;
+                    substring(ele FROM E'^(-?\\d+)(\\D|$)')::int ELSE 0 END) +
+                    (CASE WHEN tags->'natural' != 'saddle' THEN 10000 ELSE 0 END) +
+                    (CASE WHEN NULLIF (name, '') IS NOT NULL THEN 10000 ELSE 0 END)
+            ) DESC
+            )::int AS "rank"
+        FROM
+            osm_peak_point
+        WHERE
+            geometry && bbox
+     ) AS ranked_peaks
+WHERE zoom_level >= 7
+  AND (rank <= 5 OR zoom_level >= 14)
+ORDER BY "rank" ASC;
 
 $$
 LANGUAGE SQL
